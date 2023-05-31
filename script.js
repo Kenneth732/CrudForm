7f9d69fb825be19ed9f9e76b209020d758f39624
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-31 13:37:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Get the form, input fields, and book list elements
+const form = document.getElementById('bookForm');
+const titleInput = document.getElementById('titleInput');
+const authorInput = document.getElementById('authorInput');
+const bookList = document.getElementById('bookList');
+
