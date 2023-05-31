13fb5ae5c7d996538f3c61d0d155d8cf5487911f
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-31 13:43:45 +0300

    event

--- script.js
+++ script.js
@@ -47,3 +47,30 @@ function deleteBook(id) {
     renderBookList();
 }
 
+// Function to render the book list
+function renderBookList() {
+    // Clear the book list
+    bookList.innerHTML = '';
+
+    // Loop through the books array and create list items
+    books.forEach(book => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span class="book-info">${book.title} by ${book.author}</span>
+            <button class="delete-button">Delete</button>
+        `;
+
+        // Add event listener to the delete button
+        const deleteButton = li.querySelector('.delete-button');
+        deleteButton.addEventListener('click', () => deleteBook(book.id));
+
+        // Append the list item to the book list
+        bookList.appendChild(li);
+    });
+}
+
+// Add event listener to the form's submit event
+form.addEventListener('submit', addBook);
+
+// Initial rendering of the book list
+renderBookList();
